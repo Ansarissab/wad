ee4b9c7a2dfc2503382093fac816966a3581359c
Ansarissab <44458257+Ansarissab@users.noreply.github.com>
2019-01-02 18:08:35 +0500

    class activity techbox (Database)
    created database and created content dynamically

---
+++ server/db_connection.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: l1f16bscs0071
+ * Date: 1/2/2019
+ * Time: 5:06 PM
+ */
+
+$con = mysqli_connect("localhost","root","","firstdatabase");
+if(!$con){
+    die("Connectin failed");
+}
+
+?>
